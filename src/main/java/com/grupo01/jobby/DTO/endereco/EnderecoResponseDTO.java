ac3f895113d064bcc84fb9b25ea0417b3caa044f
AndersonDiasSampaio <anderson.dias.sampaio@gmail.com>
2023-07-15 15:09:41 -0300

    criado enderecoresposteDTo
    @andersondiassampaio criado enderecoresposteDTo

---
+++ src/main/java/com/grupo01/jobby/DTO/endereco/EnderecoResponseDTO.java
@@ -0,0 +1,19 @@
+package com.grupo01.jobby.DTO.endereco;
+
+import com.grupo01.jobby.model.cadastro.Cidade;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+
+@Data
+public class EnderecoResponseDTO {
+
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private Cidade cidade;
+
+}
