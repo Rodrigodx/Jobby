a9dd20760a6467fadd000a21a1d97340210a2685
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-15 13:01:20 -0300

    @Leonardo2M feat: endpoint para listar Experiencias por id

--- src/main/java/com/grupo01/jobby/services/CadastroExperienciaService.java
+++ src/main/java/com/grupo01/jobby/services/CadastroExperienciaService.java
@@ -1,6 +1,7 @@
 package com.grupo01.jobby.services;
 
 import com.grupo01.jobby.model.cadastro.CadastroExperiencia;
+import com.grupo01.jobby.model.cadastro.exception.EntityNotFoundException;
 import com.grupo01.jobby.repositories.CadastroExperienciaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,9 @@ public class CadastroExperienciaService {
 
     public List<CadastroExperiencia> findAll() {
         return cadastroExperienciaRepository.findAll();
+    }
+
+    public CadastroExperiencia findById(Long id) {
+        return cadastroExperienciaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Experiencia não cadastrada"));
     }
 }
