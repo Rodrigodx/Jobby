34a4e2d12d7f77d882e5556b4bc526628469a982
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-16 00:28:52 -0300

    @Leonardo2M feat: configuracoes basicas para o Swagger

---
+++ src/main/java/com/grupo01/jobby/config/Swagger.java
@@ -0,0 +1,20 @@
+package com.grupo01.jobby.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Jobby API para cadastro de currículo")
+                        .description("API Rest desenvolvida para o cadastro de cv para que recrutadores busquem de forma prática"));
+    }
+
+}
