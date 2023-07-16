efe8c6188b665d35ebfec55f003848ef1f80bf0e
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-16 01:30:24 -0300

    @Leonardo2M fix: colocando retorno Page nos repository

--- src/main/java/com/grupo01/jobby/repositories/CadastroExperienciaRepository.java
+++ src/main/java/com/grupo01/jobby/repositories/CadastroExperienciaRepository.java
@@ -1,14 +1,14 @@
 package com.grupo01.jobby.repositories;
 
 import com.grupo01.jobby.model.cadastro.CadastroExperiencia;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CadastroExperienciaRepository extends JpaRepository<CadastroExperiencia, Long> {
 
-    List<CadastroExperiencia> findAll();
+    Page<CadastroExperiencia> findAll(Pageable page);
 
 }
--- src/main/java/com/grupo01/jobby/repositories/CadastroRepository.java
+++ src/main/java/com/grupo01/jobby/repositories/CadastroRepository.java
@@ -1,12 +1,12 @@
 package com.grupo01.jobby.repositories;
 
 import com.grupo01.jobby.model.cadastro.Cadastro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
 
-    List<Cadastro> findAll();
+   Page<Cadastro> findAll(Pageable page);
 
 }
