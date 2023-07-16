e7834fdc82da416a1e7a0cdb5e4ec5d4920ad1ad
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-16 00:33:16 -0300

    @Leonardo2M feat: configurando ModelMapper

--- src/main/java/com/grupo01/jobby/config/SwaggerConfig.java
+++ src/main/java/com/grupo01/jobby/config/SwaggerConfig.java
@@ -1,13 +1,12 @@
 package com.grupo01.jobby.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Swagger {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
