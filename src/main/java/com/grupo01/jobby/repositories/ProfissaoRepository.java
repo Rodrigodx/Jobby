50a524cafc7fe3e7858ab1489f2bb234b97cca3e
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-15 23:43:10 -0300

    @Leonardo2M feat: adicionando paginacao no metodo findAll

--- src/main/java/com/grupo01/jobby/repositories/ProfissaoRepository.java
+++ src/main/java/com/grupo01/jobby/repositories/ProfissaoRepository.java
@@ -3,5 +3,10 @@ package com.grupo01.jobby.repositories;
 import com.grupo01.jobby.model.cadastro.Profissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfissaoRepository extends JpaRepository<Profissao, Integer> {
+
+    List<Profissao> findAll();
+
 }
